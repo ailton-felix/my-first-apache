0bcde47d331b96a179cf6a799a182d4f6b0486b4
ailton <afdlf2@gmail.com>
2023-12-13 12:28:01 -0300

    finalize apache local test

--- apache_local_test.py
+++ apache_local_test.py
@@ -1,6 +1,64 @@
 import subprocess
+import os
 
-if __name__=='__main__':
+import apache_beam as beam
 
-    subprocess.run("wget --directory-prefix=./dataset/ https://raw.githubusercontent.com/cassiobolba/Python/master/Python-Apache-Beam/voos_sample.csv", shell=True)
-    print("Hello")
+
+class MyFilter(beam.DoFn):
+    """
+    Delay-free flight filter
+    """
+    def process(self, record, *args, **kwargs):
+        if int(record[8]) > 0:
+            return [record]
+
+
+if __name__ == '__main__':
+
+    data_path = './dataset/voos_sample.csv'
+
+    if not os.path.exists(data_path):
+        # Download dataset
+        subprocess.run("wget --directory-prefix=./dataset/ https://raw.githubusercontent.com/cassiobolba/Python/master/"
+                       "Python-Apache-Beam/voos_sample.csv", shell=True)
+
+    p1 = beam.Pipeline()
+
+    # beam.CombinePerKey
+    time_delay = (
+            p1
+            | "Importar Dados Atraso" >> beam.io.ReadFromText(data_path, skip_header_lines=1)
+            | "Separar por Vírgulas Atraso" >> beam.Map(lambda record: record.split(','))
+            # voos com atraso
+            | "Pegar voos de Los Angeles com atraso" >> beam.ParDo(MyFilter())
+            # cria um dicionário
+            | "Criar par atraso" >> beam.Map(lambda record: (record[4], int(record[8])))
+            # somas dos valores de atrasos (acúmulo de atrasos)
+            | "Somar por key" >> beam.CombinePerKey(sum)
+        # |   "Mostrar Resultados" >> beam.Map(print)
+    )
+
+    # beam.combiners.Count.PerKey
+    number_of_delays = (
+            p1
+            | "Importar Dados" >> beam.io.ReadFromText(data_path, skip_header_lines=1)
+            | "Separar por Vírgulas" >> beam.Map(lambda record: record.split(','))
+            # voos com atraso
+            | "Pegar voos de Los Angeles qtd" >> beam.ParDo(MyFilter())
+            # cria um dicionário
+            | "Criar par qtd" >> beam.Map(lambda record: (record[4], int(record[8])))
+            # contando quantos atrasos tem cada aeroporto
+            | "Contar por key" >> beam.combiners.Count.PerKey()
+        # |   "Mostrar Resultados" >> beam.Map(print)
+    )
+
+    delay_table = (
+        # montando um dicionário com os resultados das duas pCollection acima
+        {'number_of_delays': number_of_delays, 'time_delay': time_delay}
+        # agrupando as duas pCollection em função de suas keys (semelhante a
+        # operação JOIN do SQL)
+        | "Group By" >> beam.CoGroupByKey()
+        | beam.Map(print)
+    )
+
+    p1.run()
